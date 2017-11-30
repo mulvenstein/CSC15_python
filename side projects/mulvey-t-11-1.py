1bbe1cf3c692d8b62d0c202ab5044da2d97757a8
tom mulvey <tommulvey5567@gmail.com>
2017-11-29 23:09:30 -0500

    Update mulvey-t-11-1.py

--- side projects/mulvey-t-11-1.py
+++ side projects/mulvey-t-11-1.py
@@ -3,137 +3,229 @@
 #date : 11/28
 #vers 0.0.1
 #purpose : create a tic tac toe game vs computer,
-#manipulate lists, continuously print/update,
+#          and make a semi smart AI
 
-#steps : human plays -> check win -> computer plays
-# update between steps.
+'''there is some really janky code, especially with the computer "ai" 
+   I made it so the computer was beatable, I could add forking to make it impossible
+   or change prioty of mid control. 
+   
+   Classes were kind of useless, but if i wanted to expand to human vs human they wouuld be useful. 
+   Since it is only one instance of human vs comp every time they are not utilized well, but it was good practice
+   with classes beause we have not had a dedicated lab for it yet. 
+   
+   TODO: Rename variables, clean up the spilt spaghetti code, never look at computer moves again...'''
+
+
 from random import *
-import numpy as np
-#a = [[1,4], [2,6], [3,9]]
-#print(np.matrix(a)) it works.
 
-def cls(): #this is realllly eh
-   print("\n" * 100)
+class ttt_board(): #let's make some OOP :D
+
+   def __init__(self) : 
+      self.board = {1:'', 2:'', 3:'', 4:'', 5:'', 6:'', 7:'', 8:'', 9:''}
+      self.row_list = [ [1,2,3], [4,5,6], [7,8,9] ]
+      self.col_list = [ [1,4,7], [2,5,8], [3,6,9] ]
+      self.slant_list = [ [1,5,9], [3,5,7] ]
+      #the row, col, and slant list are used when checking if someone has won. Will only check the 3 spots in the specif row col or slant
+      self.row_list_iterator = 0
+      self.col_list_iterator = 0
+   
+   def reset(self): #resets game.
+      self.board = {1:'', 2:'', 3:'', 4:'', 5:'', 6:'', 7:'', 8:'', 9:''}
+   
+   def check_spot(self, position) : #checks to see if desired spot is available.
+      if self.board[position] != '' :
+         return True
+   
+   def is_board_full(self) : 
+      empty = ''
+      return not any(spot == empty for spot in self.board.values()) #if +1 spot is not empty, returns false, otherwise true
+
+   def checK_game_over(self, position): #check slanted victroies always.
+       #first find the row and col position is in
+      self.row_list_iterator = 0
+      self.col_list_iterator = 0
+      while True : #find row.
+         if position in self.row_list[self.row_list_iterator]:
+            break
+         else:
+            self.row_list_iterator += 1
+
+      while True : #find col.
+         if position in self.col_list[self.col_list_iterator]:
+            break
+         else:
+            self.col_list_iterator += 1
+   
+      #the iterators for row and col are the col and row the num is in. If pos
+      #is 4 or 6 do not check the slants. else see if the values of the keys
+      #in the row and col ar all the same and make sure they are all not empty.
+      #if they are all 'c' the comp wins. if all 'h' then human wins!
+      
+      rl = self.row_list[self.row_list_iterator]
+      cl = self.col_list[self.col_list_iterator]
+      sl1 = self.slant_list[0]
+      sl2 = self.slant_list[1]
+      
+      if (self.board[rl[0]] == self.board[rl[1]] == self.board[rl[2]]) and self.board[rl[0]] != '': #all chars in row are same and not empty
+         #if all board[i] for i in rowlist[iter] == board[pos]
+         if self.board[rl[0]] == 'c' :
+            return "computer"
+         else :
+            return "human"
+      elif (self.board[cl[0]] == self.board[cl[1]] == self.board[cl[2]]) and self.board[cl[0]] != '': #all chars in col are same + not empty
+         if self.board[cl[0]] == 'c' :
+            return "computer"
+         else :
+            return "human"
+      elif (self.board[sl1[0]] == self.board[sl1[1]] == self.board[sl1[2]]) and self.board[sl1[0]] != '':  #checks first slant
+         if self.board[sl1[0]] == 'c' :
+            return "computer"
+         else :
+            return "human"
+      elif (self.board[sl2[0]] == self.board[sl2[1]] == self.board[sl2[2]]) and self.board[sl2[0]] != '': #checks second slant
+         if self.board[sl2[0]] == 'c' :
+            return "computer"
+         else :
+            return "human"
+      else : #no one has wonnnered
+         return "continue"
+         #THIS IS SOME REAL SPAGHETTI CODE> SHOULD FIND BETTER WAY TO CHECK!!!!!!!!
+   def print_board(self):
+      print("\nGame Board :")
+      print("|", end='')
+      for i in range(1,4) :
+         if self.board[i] == '':
+            print(" ", i, "|", end='')
+         else:
+            print(" ", self.board[i],   "|", end='')
+      print("\n---------------")
+
+      print("|", end='')
+      for i in range(4,7) :
+         if self.board[i] == '':
+            print(" ", i, "|", end='')
+         else:
+            print(" ", self.board[i], "|", end='')
+      print("\n----------------")
+
+      print("|", end='')
+      for i in range(7,10) :
+         if self.board[i] == '':
+            print(" ", i, "|", end='')
+         else:
+            print(" ", self.board[i], "|", end='')
+      print("\n")
+      return
+
+#-----class is done, add to anothe file then import???--------#
+
+#now that i think of it my classes have no actual use....
+#it is too late so I am going to contiue anyways....
+#woops...
+
+def cls():
+   print("\n" * 100) #lol this is high level programming
    return
 
-def initialize_board(board) :
-   #inits the board for use. empty board
-   board = [['','',''],['','',''],['','','']]
-   return board
+def human_turn(game_on):
+   position = int(input("Hello Human. Would Spot would you like ? : "))
+   while game_on.check_spot(position) == True or (position < 0 or position > 10): #runs if the spot is occupied
+      position = int(input("Try another spot, human. : "))
+   game_on.board[position] = 'h'
+   return position
 
-def display(board):
-   #displays current status of board, ran after every move
-   #numpy print?
-   #print("\n\n\n\n\n\n\n\n\n\n\n")
-   print(" | (1,1) | (1,2) | (1,3) |\n",\
-         "| (2,1) | (2,2) | (2,3) |\n",\
-         "| (3,1) | (3,2) | (3,3) |\n",)
+def computer_turn(board):
+   # position = randint(1,9)
+   # while game_on.check_spot(position) == True :
+   #    position = randint(1,9)
+   # game_on.board[position] = 'c'
+   # return position
+   # used to test if game worked ^
 
-   print(np.matrix(board))
-   return
+   # win if u have 2 in a row
+   # bock if opponent hsa 2 in a row
+   # fork - create an oprotunity where u can win in 2 way
+   # block opponent fork
+   #  opt 1 : create 2 in a row to force opp into defending
+   #  opt 2 : if ther is a config where human can fork, block it
+   # center 
+   # if op is in op corner, take that corner
+   # play empty corner
+   # play empty side
+   #alter these priorities to change difficulty level??
+   
+   def can_win(board, sign, i):
+      test = ttt_board()
+      for x in board :
+         if x in test.board :
+            test.board[x] = board[x]
+      test.board[i] = sign
+      return test.checK_game_over(i)
+   #coomp win ?
+   for  i in [1,2,3,4,5,6,7,8,9]:
+      if board[i] == '':
+         call = can_win(board, 'c', i)
+         if call == "computer":
+            return i
+   #player win
+   for  i in [1,2,3,4,5,6,7,8,9]:
+      if board[i] == '':
+         call = can_win(board, 'h', i)
+         print(board, call)
+         if call == "human":
+            return i
+   #take corner
+   for i in [1,3,7,9]:
+      if board[i] == '' :
+         return i
+   #center
+   if board[5] == '' :
+      return 5
+   
+   #side play
+   for i in [2,4,6,8]:
+      if board[i] == '' :
+         return i
+   
+   #need to add fork, brute force ? prob best. research minimax more
 
-def human_move(board):
-   #human's turn to place his mark.
-   #display grid after.
-   print("Humans Turn>> Grid Location? ")
-   row = int(input("row? : "))
-   col = int(input("col? : "))
-   check = check_grid(row, col, board)
+def driver(): #driver function
+   print("welcome to tic tac toe\n")
+   game_on = ttt_board() 
 
-   while check == 0:
-      cls()
-      display(board)
-      print("invalid input. Try again.")
-      row = int(input("row? : "))
-      col = int(input("col? : "))
-      check = check_grid(row, col, board)
-
-   board[row-1][col-1] = 'X'
-   ans, char = check_game(row, col, board, 'X')
-
-   return board, ans, char
-
-def computer_move(board):
-   #computer's turn
-   #MI..?
-   print("comps move.")
-   row = randint(0,3)
-   col = randint(0,3)
-   check = check_grid(row, col, board)
-   while check == 0 :
-      row = randint(0,3)
-      col = randint(0,3)
-      check = check_grid(row, col, board)
-   board[row-1][col-1] = 'O'
-   ans, char = check_game(row, col, board, 'O')
-   return board, ans, char
-
-def check_game(row, col, board, char):
-   #comp win, human win, tie?
-   #check all horizantals on row
-   #check all verts on col
-   #always check slanteds
-   ans = "maybe"
-   #slanteds
-   if board[0][0]== char and board[1][1] == char and board[2][2] == char : #someone wons!
-      return "yes", char
-   elif board[0][2] == char and board[1][1] == char and board[2][0] == char : #someone wons!
-      return "yes", char
-   else :
-       ans = "no"
-
-   #horizontals
-   if board[row-1][0] == char and board[row-1][1] == char and board[row-1][2] == char :#hoiz win
-      return "yes", char
-   else :
-      ans = "no"
-
-   #vertz
-   if board[0][col-1] == char and board[1][col-1] == char and board[2][col-1] == char : #wons!
-      return "yes", char
-   else :
-      ans = "no"
-
-   return ans, char
-
-def check_grid(row, col, board):
-   #checks spot + grid if valid
-   check = 2
-   if 0 < row < 4 and 0 < col < 4 :
-      if board[row-1][col-1] == '' :
-         check = 1
-      else :
-         check = 0
-   else :
-      check = 0
-
-   return check
-
-def main():
-   #driver function.
-   board = list()
-   board = initialize_board(board)
-   #print(np.matrix(board))
-   print("Tic Tac Toe!\n")
-   while True :
-      #display, human, check_valid, check_game, display, comp, check_valid, check_game
-      display(board)
-      board, ans, char = human_move(board)
-      if ans == "yes" :
-         print(char, " has won my dude!")
+   while True : #run turn base fun
+      #show screen -> human plays -> check spot, game status -> cls -> computer goes -> check spot/game status -> cls
+      game_on.print_board()
+      position = human_turn(game_on)
+      status = game_on.checK_game_over(position)
+      if status != "continue" :
+         break
+      cont = game_on.is_board_full()
+      if cont:
+         status =" tie "
          break
       cls()
-      display(board)
-
-      board, ans, char = computer_move(board)
-      if ans == "yes" :
-         print(char, " has won my dude!")
+      bored = game_on.board
+      position = computer_turn(bored)
+      game_on.board[position] = 'c'
+      status = game_on.checK_game_over(position)
+      if status != "continue" :
+         break
+      cont = game_on.is_board_full()
+      if cont:
+         status =" tie "
          break
       cls()
 
-   print("final board : \n")
-   display(board)
-   return
+   cls()
+   game_on.print_board()
+   if status == "human" :
+      print("human wonnered")
+   elif status == " tie ":
+      print("tis a tie")
+   else :
+      print("computer wonnered")
 
-#----#
-main()
+   return
+#-----#
+driver()
